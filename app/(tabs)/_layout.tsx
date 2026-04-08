7a9c84b139c141838abaaf4627faebe8c97376f0
Guilherme <a2023135558@alumni.iscac.pt>
2026-04-08 17:48:17 +0100

    1ª entrega

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,64 @@
+import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
+    <Tabs 
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
+        headerShown: false, // Esconde o cabeçalho no topo
+        tabBarShowLabel: false, // Esconde os textos debaixo dos ícones
+        tabBarActiveTintColor: 'rgb(255, 255, 255)', // Cor do ícone selecionado
+        tabBarInactiveTintColor: 'rgb(103, 103, 103)', // Cor dos ícones não selecionados
+        tabBarStyle: {
+          backgroundColor: 'rgb(0,0,0)',
+          borderTopWidth: 1,
+          borderTopColor: 'rgb(0, 0, 0)',
+          height: 60,
+          marginBottom: 20,
+        }
+      }}
+    >
+
+      {/* ABA 1: HOME */}
       <Tabs.Screen
-        name="index"
+        name="home"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <Ionicons name="home" size={26} color={color} />,
         }}
       />
+      
+      {/* ABA 2: Favorito */}
+        <Tabs.Screen
+          name="favorites"
+          options={{
+            tabBarIcon: ({ color }) => <Ionicons name="heart-outline" size={26} color={color} />,
+          }}
+        />
+      
+      {/* ABA 3: CARRINHO */}
       <Tabs.Screen
-        name="explore"
+        name="cart"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <Ionicons name="cart-outline" size={26} color={color} />,
         }}
       />
+      
+      {/* ABA 4: CHAT */}
+      <Tabs.Screen
+        name="chat"
+        options={{
+          tabBarIcon: ({ color }) => <Ionicons name="chatbubble-outline" size={26} color={color} />,
+        }}
+      />
+      
+      {/* ABA 5: PERFIL */}
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ color }) => <Ionicons name="person-outline" size={26} color={color} />,
+        }}
+      />
+
     </Tabs>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,67 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { DarkTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, View } from 'react-native';
 import 'react-native-reanimated';
+import "../global.css";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+// Importa a função (ajusta o caminho para onde criaste o ficheiro)
+// Se seguiste o passo anterior, será '../src/services/database'
+import { setupDatabase } from '../src/services/database';
+
+// Impede que o ecrã de Splash nativo desapareça antes da BD estar pronta
+SplashScreen.preventAutoHideAsync();
 
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const [dbLoaded, setDbLoaded] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Carrega a BD do SQLite
+        await setupDatabase();
+        setDbLoaded(true);
+      } catch (e) {
+        console.warn("Erro ao carregar a BD:", e);
+      } finally {
+        // Esconde o Splash Screen nativo
+        await SplashScreen.hideAsync();
+      }
+    }
+
+    prepare();
+  }, []);
+
+  // Enquanto a BD não carrega, mostra o loading com a tua cor oficial
+  if (!dbLoaded) {
+    return (
+      <View style={{ 
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        backgroundColor: 'rgb(58, 79, 92)' 
+      }}>
+        <ActivityIndicator size="large" color="white" />
+      </View>
+    );
+  }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+    <ThemeProvider value={DarkTheme}>
+      {/* headerShown: false para não aparecer o título feio no topo */}
+      <Stack screenOptions={{ headerShown: false, animation: 'none' }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="login" />
+        <Stack.Screen name="register" />
       </Stack>
-      <StatusBar style="auto" />
+      {/* StatusBar 'light' para ser branco e ver-se no fundo azul escuro */}
+      <StatusBar style="light" />
     </ThemeProvider>
   );
 }
