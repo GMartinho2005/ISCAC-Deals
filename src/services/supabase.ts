20bd4e375449fae948380c67ad071b9f8aa8f9b4
Guilherme <a2023135558@alumni.iscac.pt>
2026-05-07 16:07:42 +0100

    2ª entrega

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,9 +1,9 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://esynhuqedmucenvriakc.supabase.co';
+const SUPABASE_URL = '';
 const SUPABASE_ANON_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzeW5odXFlZG11Y2VudnJpYWtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzMzg5NjUsImV4cCI6MjA5MTkxNDk2NX0.AH8oC8hDuK4yzNqwZ7oIYQH22GdD3vBOHgCjmXsJ7oA';
+  '';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
